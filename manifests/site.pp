debb72f74ff47501951f3d30e4683d059caaaaa5
travishawken <72138135+travishawken@users.noreply.github.com>
2021-02-16 13:49:54 +1100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,11 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner   => 'root',
   }
 }
 node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
     ensure => file,
-    content => “Welcome to ${fqdn}”,
-    owner => 'root',
+    content => $fqdn,
   }
 }
 node /^web/ { 
